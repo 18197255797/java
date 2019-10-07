72a0242da516b0086f965097d81d27c4789ead6e
sandy <49701536+18197255797@users.noreply.github.com>
2019-10-07 22:53:36 +0800

    Add files via upload

---
+++ Test11.java
@@ -0,0 +1,16 @@
+public class Test11{
+    public static void main(String[] args) {
+       // System.out.println("李"+"国"+"秀"); 
+        int a = 1;
+        double b = 2.2;
+        char c = 'a';
+        boolean d,e,f;
+		d=true;
+		e=false;
+		f=d&e;   
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(f);
+    }
+}
