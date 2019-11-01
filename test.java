a8d2b3112940e80b9b5eff6aaa4d9c24ddf70c67
sandy <49701536+18197255797@users.noreply.github.com>
2019-11-01 10:31:24 +0800

    array

--- test.java
+++ test.java
@@ -1,24 +1,61 @@
-package Exception;
+package practice;
+
+import java.nio.charset.MalformedInputException;
 
 public class test {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3};
-        try{System.out.println("frist");
-            arr = null;
-            System.out.println(arr[4]);
-            System.out.println("second");
-        }catch(ArrayIndexOutOfBoundsException e) {
-            // 打印出现异常的调用栈
-            System.out.println("数组下标异常");
-            e.printStackTrace();
-       }
-        catch(NullPointerException e){
-            System.out.println("空指针异常");
-            e.printStackTrace();
-        }
-//        System.out.println("third");
-        finally {
-            System.out.println("finally ");
-        }
+        
     }
 }
+
+//public static void main(String[] args) {
+//    //参数传数组类型（引用类型）
+////函数的内部发生变换，函数的外部也会发生变换
+////此时的arr是一个引用，当传参的时候，是按照引用传参的
+//        int[] arr = new int[]{1,2,3};
+//        fun(arr);
+//        System.out.println("arr[0]="+arr[0]);
+//    }
+//    public static  void fun(int a[]){
+//        a[0] = 10;
+//        System.out.println("a[0]="+a[0]);
+//    }
+//}
+//1.参数传内置参数
+//    public static void main(String[] args) {
+//        int num  = 0;
+//        fun(num);
+//        System.out.println("num ="+num);
+//    }
+//    //修改形参x的值，不会影响实参的num的值
+//    public static void fun(int x){
+//        x = 10;
+//        System.out.println("x="+x);
+//    }
+//}
+   // public static void main(String[] args) {
+//        int[] arr = {1, 2, 3};
+//        printArray(arr);
+//    }
+//}
+//    public static void printArray(int[] a){
+//        for(int x:a){
+//            System.out.println(x);
+//        }
+//    }
+//}
+//        int[] arr1 = new int[3];
+//        System.out.println(arr2[0]);
+//        System.out.println(arr2[1]);
+//        System.out.println(arr2[2]);
+//        System.out.println("length"+arr2.length);
+//        arr2[2]=7;
+//        for(int x:arr2){
+//            System.out.println(x);
+//        }for(int x :arr1){
+//            System.out.println(x);
+//        }
+//
+//        //System.out.println(arr2[]);
+//    }
+//}
