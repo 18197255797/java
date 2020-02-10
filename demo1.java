c58238e11490062e8be3d9a8f304b7dd961c52d6
sandy <49701536+18197255797@users.noreply.github.com>
2020-02-10 17:12:18 +0800

    Add files via upload

--- demo1.java
+++ demo1.java
@@ -1,66 +1,49 @@
-package package11;
-import java.util.Stack;
+package first;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class demo1 {
-    public static void quickSort(int[] array) {
+//    public boolean isPalindrome(String s) {
+//        int a=s.length();
+//        if(a==0){
+//            return true;
+//        }
+//        //char []str = s.toCharArray();
+//        int left=0;
+//        int right = a-1;
+//
+//        for(;left<right;){
+//            if(Character.isLetterOrDigit(s.charAt(left))){
+//                left++;
+//                continue;
+//            }
+//            if(Character.isLetterOrDigit(s.charAt(right))){
+//                right--;
+//                continue;
+//            }
+//            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+//                return false;
+//            }
+//
+//        }
+//        while(left==right){
+//            return true;
+//        }
 
-        quickSortInternal(array, 0, array.length - 1);
-    }
-    // [left, right] 为待排序区间
-    private static void quickSortInternal(int[] array, int left, int right) {
-        if (left == right) {
-            return;
-        }
-        if (left > right) {
-            return;
-        }
-        // 最简单的选择基准值的方式，选择 array[left] 作为基准值
-        // pivotIndex 代表基准值最终停留的下标
-        int pivotIndex = partition(array,left, right);
-        // [left, pivotIndex - 1] 都是小于等于基准值的
-        // [pivotIndex + 1, right] 都是大于等于基准值的
-        quickSortInternal(array, left, pivotIndex - 1);
-        quickSortInternal(array, pivotIndex + 1, right);
-    }
-    private static int partition(int[] array, int left, int right) {
-        int i = left;
-        int j = right;
-        int pivot = array[left];
-        while (i < j) {
-            while (i < j && array[j] >= pivot) {
-                j--;
-            }
-            while (i < j && array[i] <= pivot) {
-                i++;
-            }
 
-            swap(array, i, j);
-        }
-        swap(array, i, left);
-        return i;
+    public static void main(String args[]){
+        int num = 10;
+        System.out.println(test(num));
     }
-    public static void swap(int []array,int x, int y) {
-    int tmp = array[x];
-    array[x] = array[y];
-    array[y] = tmp;
-    }
-    //非递归版本的快速排序
-    public static void quickSortByLoop(int[] array){
-        //先创建一个栈，栈里面存的是待处理区间的下标、
-        Stack<Integer> stack = new Stack<>();
-        stack.push(array.length-1);
-        stack.push(0);
-        while(!stack.isEmpty()){
-            //取出栈顶元素
-            int left = stack.pop();
-            int right = stack.pop();
-            if (left >= right) {
-                continue;
-            }
-            int pivotIndex = partition(array, left, right);
-            stack.push(right);
-            stack.push(pivotIndex + 1);
-            stack.push(pivotIndex - 1);
-            stack.push(left);
-        }
+    public static int test(int b){
+        try { b += 10;
+            return b;
+        }catch(RuntimeException e){
+
+        }catch(Exception e2){
+
+        } finally{ b += 10; return b; }
     }
 }
+
